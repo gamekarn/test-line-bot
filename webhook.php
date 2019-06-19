41963450ce8dc6403430081d3a4edf96d546b239
gamekarn <51943506+gamekarn@users.noreply.github.com>
2019-06-19 09:15:39 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -94,7 +94,7 @@
         $jsonInfoGold = json_decode(getInfoGold());
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = 'ราคาทองตอนนี้ ' . $jsonInfoGold[0];
+        $arrayPostData['messages'][0]['text'] = 'ราคาทองตอนนี้ ' . getInfoGold();
         replyMsg($arrayHeader,$arrayPostData);
     }
 
